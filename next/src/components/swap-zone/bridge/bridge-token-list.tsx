9119ffea7ac6805c47ce869801b7838b76d9869c
scluzive-bastine <toshfuture@gmail.com>
2025-08-01 21:18:26 +0100

    Make bridge list responsive

--- next/src/components/swap-zone/bridge/bridge-token-list.tsx
+++ next/src/components/swap-zone/bridge/bridge-token-list.tsx
@@ -5,17 +5,34 @@ import Address from "@/components/elements/address"
 import Symbol from "@/components/elements/symbol"
 import TokenName from "@/components/elements/token-name"
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+} from "@/components/ui/drawer"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { useAppContext } from "@/context/app-context"
 import { useBridge } from "@/context/bridge-context"
 import { cn } from "@/lib/utils"
 import { DialogDescription } from "@radix-ui/react-dialog"
+import { ChevronDownIcon } from "lucide-react"
 import { useState } from "react"
+import { useMediaQuery } from "usehooks-ts"
 import { POPULAR_CHAINS } from "./constants"
 
 const BridgeTokenList = () => {
   const { state, toggleBridgeTokenList } = useAppContext()
+  const isMobile = useMediaQuery("(max-width: 768px)")
 
   const open = state.bridgeTokenListOpen
 
@@ -25,16 +42,18 @@ const BridgeTokenList = () => {
 
   return (
     <>
-      <div className='hidden md:flex'>
-        <ListDialog open={open} onOpenChange={onOpenChange} />
-      </div>
+      {isMobile ? (
+        <MobileListDialog open={open} onOpenChange={onOpenChange} />
+      ) : (
+        <DesktopListDialog open={open} onOpenChange={onOpenChange} />
+      )}
     </>
   )
 }
 
 export default BridgeTokenList
 
-const ListDialog = ({ open, onOpenChange }: { open: boolean; onOpenChange: () => void }) => {
+const DesktopListDialog = ({ open, onOpenChange }: { open: boolean; onOpenChange: () => void }) => {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent
@@ -43,43 +62,30 @@ const ListDialog = ({ open, onOpenChange }: { open: boolean; onOpenChange: () =>
           opacity-0 scale-95
           data-[state=open]:opacity-100 data-[state=open]:scale-100
           sm:max-w-2xl bg-white dark:bg-neutral-950
+          hidden md:block
         '
       >
         <DialogHeader>
           <DialogTitle className='text-base font-medium'>Select a token</DialogTitle>
           <DialogDescription className='sr-only'>Select a token to swap</DialogDescription>
         </DialogHeader>
-        {/* <div className='flex flex-col gap-4'>
-            <Input
-              placeholder='Search for a token'
-              className='h-12 capitalize'
-              onChange={handleSearch}
-            />
-
-            <ScrollArea className='h-full w-full max-h-[350px] space-y-2'>
-              {filteredCurrencies?.map((currency, index) => (
-                <div
-                  key={index}
-                  className='mb-2 w-full border border-zinc-200 dark:border-zinc-900 bg-zinc-100 dark:bg-neutral-950 flex gap-2 items-center px-3 py-2 rounded-xl hover:bg-zinc-200 dark:hover:bg-neutral-900 cursor-pointer transition-all duration-300'
-                  onClick={() => handleClick(currency)}
-                >
-                  {currency.image && <TokenLogo src={currency.image} alt={currency.ticker} />}
-                  <div className='flex flex-col gap-1'>
-                    <div className='flex gap-2 items-center'>
-                      <Symbol symbol={currency.ticker} />
-                      <Badge>
-                        <span className='text-xs uppercase font-medium'>{currency.network}</span>
-                      </Badge>
-                    </div>
-                    <TokenName name={currency.name} />
-                  </div>
-                </div>
-              ))}
-            </ScrollArea>
-          </div> */}
         <ListDetails />
       </DialogContent>
     </Dialog>
+  )
+}
+
+const MobileListDialog = ({ open, onOpenChange }: { open: boolean; onOpenChange: () => void }) => {
+  return (
+    <Drawer open={open} onOpenChange={onOpenChange}>
+      <DrawerContent className='w-full min-h-[92vh] data-[vaul-drawer-direction=bottom]:max-h-[92vh] rounded-t-3xl'>
+        <DrawerHeader>
+          <DrawerTitle className='text-base font-medium'>Select a token</DrawerTitle>
+          <DrawerDescription className='sr-only'>Select a token to swap</DrawerDescription>
+        </DrawerHeader>
+        <ListDetails />
+      </DrawerContent>
+    </Drawer>
   )
 }
 
@@ -124,77 +130,124 @@ const ListDetails = () => {
 
   return (
     <div className='grid grid-cols-12 gap-2'>
-      <div className='col-span-12 md:col-span-4 bg-neutral-100 dark:bg-neutral-900 rounded-lg p-4 flex flex-col gap-2'>
-        <Input
-          placeholder='Search chains'
-          className='capitalize bg-white focus-visible:ring-0'
-          onChange={handleSearchChains}
-        />
-        <ScrollArea className='md:max-h-[400px] h-full'>
-          <div className='flex flex-col gap-3'>
-            <div
-              className={cn(
-                "text-sm font-medium text-black dark:text-white cursor-pointer h-10 flex gap-2 items-center hover:bg-zinc-200/50 dark:hover:bg-neutral-950 rounded-md p-2 transition-all duration-300",
-                !activeChain && "bg-zinc-200 dark:bg-neutral-950 rounded-md p-2"
-              )}
+      <div className='col-span-12 md:col-span-4 bg-neutral-100 dark:bg-neutral-900 rounded-lg p-4 relative z-10 md:z-0'>
+        <div className='hidden md:flex flex-col gap-2 h-full'>
+          <SearchChains handleSearchChains={handleSearchChains} />
+          <ScrollArea className='md:max-h-[400px] h-full'>
+            <div className='flex flex-col gap-3'>
+              <SelectChain
+                chain={null}
+                activeChain={activeChain}
+                handleSelectChain={handleSelectChain}
+              />
+              <div className='flex flex-col gap-2 mt-2'>
+                <small className='text-xs text-zinc-700 dark:text-zinc-300'>Popular chains</small>
+                <div className='flex flex-col gap-1'>
+                  {popularChains.map((chain) => (
+                    <ChainsComponent
+                      key={chain.id}
+                      chain={chain}
+                      activeChain={activeChain}
+                      handleSelectChain={handleSelectChain}
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className='flex flex-col gap-2 mt-2'>
+                <small className='text-xs text-zinc-700 dark:text-zinc-300'>Chains A-Z</small>
+                <div className='flex flex-col gap-1'>
+                  {filteredChains
+                    .sort((a, b) => a.name.localeCompare(b.name))
+                    .map((chain) => (
+                      <ChainsComponent
+                        key={chain.id}
+                        chain={chain}
+                        activeChain={activeChain}
+                        handleSelectChain={handleSelectChain}
+                      />
+                    ))}
+                </div>
+              </div>
+            </div>
+          </ScrollArea>
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger className='w-full md:hidden group' data-state='closed'>
+            {activeChain ? (
+              <SelectChain
+                chain={activeChain}
+                activeChain={activeChain}
+                handleSelectChain={handleSelectChain}
+              />
+            ) : (
+              <SelectChain
+                chain={null}
+                activeChain={activeChain}
+                handleSelectChain={handleSelectChain}
+              />
+            )}
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className='rounded-lg relative min-w-[calc(100vw-2rem)] max-h-[calc(25rem)] w-full bg-white dark:bg-neutral-950 p-0 shadow-none'>
+            <div className='rounded-t-lg fixed top-0 left-0 right-0 mx-auto w-[calc(100%-2px)] bg-white dark:bg-neutral-950 p-4 z-10'>
+              <SearchChains handleSearchChains={handleSearchChains} />
+            </div>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className='justify-start w-full focus:bg-transparent rounded-none'
               onClick={() => handleSelectChain(null)}
             >
-              <div className='h-6 w-6 bg-gradient-to-br from-secondary-custom via-emerald-400 to-blue-600 rounded-sm' />
-              All chains
-            </div>
-            <div className='flex flex-col gap-2 mt-2'>
-              <small className='text-xs text-zinc-700 dark:text-zinc-300'>Popular chains</small>
-              <div className='flex flex-col gap-1'>
-                {popularChains.map((chain) => (
-                  <div
-                    key={chain.id}
-                    className={cn(
-                      "flex gap-2 items-center cursor-pointer hover:bg-zinc-200/50 dark:hover:bg-neutral-950 rounded-md p-2 transition-all duration-300",
-                      activeChain?.id === chain.id && "bg-zinc-200 dark:bg-neutral-950"
-                    )}
-                    onClick={() => handleSelectChain(chain)}
-                  >
-                    <ChainImage chain={chain} />
-                    <p className='text-sm font-medium capitalize text-black dark:text-white'>
-                      {chain.name}
-                    </p>
-                  </div>
-                ))}
-              </div>
-            </div>
-            <div className='flex flex-col gap-2 mt-2'>
-              <small className='text-xs text-zinc-700 dark:text-zinc-300'>Chains A-Z</small>
-              <div className='flex flex-col gap-1'>
-                {filteredChains
-                  .sort((a, b) => a.name.localeCompare(b.name))
-                  .map((chain) => (
-                    <div
+              <SelectChain
+                chain={null}
+                activeChain={activeChain}
+                handleSelectChain={handleSelectChain}
+                className='w-full'
+              />
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild className='w-full p-2 focus:bg-transparent rounded-none'>
+              <div className='flex flex-col gap-2 w-full justify-start items-start'>
+                <small className='text-xs text-zinc-700 dark:text-zinc-300'>Popular chains</small>
+                <div className='flex flex-col gap-1 w-full'>
+                  {popularChains.map((chain) => (
+                    <ChainsComponent
                       key={chain.id}
-                      className={cn(
-                        "flex gap-2 items-center cursor-pointer hover:bg-zinc-200/50 dark:hover:bg-neutral-950 rounded-md p-2 transition-all duration-300",
-                        activeChain?.id === chain.id && "bg-zinc-200 dark:bg-neutral-950"
-                      )}
-                      onClick={() => handleSelectChain(chain)}
-                    >
-                      <ChainImage chain={chain} />
-                      <p className='text-sm font-medium capitalize text-black dark:text-white'>
-                        {chain.name}
-                      </p>
-                    </div>
+                      chain={chain}
+                      activeChain={activeChain}
+                      handleSelectChain={handleSelectChain}
+                      className='w-full'
+                    />
                   ))}
+                </div>
               </div>
-            </div>
-          </div>
-        </ScrollArea>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild className='w-full p-2 focus:bg-transparent rounded-none'>
+              <div className='flex flex-col gap-2 w-full justify-start items-start'>
+                <small className='text-xs text-zinc-700 dark:text-zinc-300'>Chains A-Z</small>
+                <div className='flex flex-col gap-1 w-full'>
+                  {filteredChains
+                    .sort((a, b) => a.name.localeCompare(b.name))
+                    .map((chain) => (
+                      <ChainsComponent
+                        key={chain.id}
+                        chain={chain}
+                        activeChain={activeChain}
+                        handleSelectChain={handleSelectChain}
+                        className='w-full'
+                      />
+                    ))}
+                </div>
+              </div>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
-      <div className='col-span-12 md:col-span-8 flex flex-col gap-2'>
+      <div className='relative col-span-12 md:col-span-8 flex flex-col gap-2 p-4 md:p-0 h-full'>
         <Input
           placeholder='Search tokens or paste address'
-          className='capitalize focus-visible:ring-0'
+          className='capitalize focus-visible:ring-0 h-10 md:h-12'
           onChange={handleSearchTokens}
         />
-        <ScrollArea className='md:max-h-[400px] h-full'>
-          <div className='flex flex-col gap-2'>
+        <ScrollArea className=' max-h-[400px] md:max-h-[420px] h-full'>
+          <div className='flex flex-col gap-2 h-full'>
             {activeChain ? (
               <FeaturedTokens
                 tokens={activeChain.featuredTokens}
@@ -333,5 +386,80 @@ const FeaturedTokens = ({
         </div>
       ))}
     </div>
+  )
+}
+
+const ChainsComponent = ({
+  chain,
+  activeChain,
+  handleSelectChain,
+  className,
+}: {
+  chain: Chain
+  activeChain: Chain | null
+  handleSelectChain: (chain: Chain | null) => void
+  className?: string
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex gap-2 items-center cursor-pointer hover:bg-zinc-200/50 dark:hover:bg-neutral-950 rounded-md p-2 transition-all duration-300",
+        activeChain?.id === chain.id && "bg-zinc-200 dark:bg-neutral-950",
+        className
+      )}
+      onClick={() => handleSelectChain(chain)}
+    >
+      <ChainImage chain={chain} />
+      <p className='text-sm font-medium capitalize text-black dark:text-white'>{chain.name}</p>
+    </div>
+  )
+}
+
+const SelectChain = ({
+  chain,
+  activeChain,
+  handleSelectChain,
+  className,
+}: {
+  chain: Chain | null
+  activeChain: Chain | null
+  handleSelectChain: (chain: Chain | null) => void
+  className?: string
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex justify-between items-center cursor-pointer hover:bg-zinc-200/50 dark:hover:bg-neutral-950 rounded-md p-2 transition-all duration-300",
+        activeChain?.id === chain?.id && "bg-zinc-200 dark:bg-neutral-950",
+        className
+      )}
+      onClick={() => handleSelectChain(chain)}
+    >
+      <div className='flex gap-2 items-center'>
+        {chain ? (
+          <ChainImage chain={chain} />
+        ) : (
+          <div className='h-6 w-6 bg-gradient-to-br from-secondary-custom via-emerald-400 to-blue-600 rounded-sm' />
+        )}
+        <p className='text-sm font-medium capitalize text-black dark:text-white'>
+          {chain ? chain.name : "All chains"}
+        </p>
+      </div>
+      <ChevronDownIcon className='md:hidden w-4 h-4 text-zinc-500 dark:text-zinc-400 transition-all duration-300 group-data-[state=open]:rotate-180' />
+    </div>
+  )
+}
+
+const SearchChains = ({
+  handleSearchChains,
+}: {
+  handleSearchChains: (e: React.ChangeEvent<HTMLInputElement>) => void
+}) => {
+  return (
+    <Input
+      placeholder='Search chains'
+      className='capitalize bg-white focus-visible:ring-0'
+      onChange={handleSearchChains}
+    />
   )
 }
